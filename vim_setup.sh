55417f9557edb31e570705b5a0db47af3a706909
xylafur <jkesleyrichardson@gmail.com>
2018-11-25 16:00:34 -0500

    Adding vim setup

---
+++ vim_setup.sh
@@ -0,0 +1,18 @@
+OS="$(cat /etc/os-release  | grep -E "^ID=.*" | cut -d'=' -f2)"
+
+if [ $OS = 'fedora' ]; then
+    INSTALL="dnf install"
+else
+    echo "Unknown linux distro!"
+    exit
+fi
+
+sudo $INSTALL  vim
+cp .vimrc ~/.vimrc
+mkdir ~/.vim
+mkdir ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir ~/.vim/pack
+mkdir ~/.vim/pack/vendor
+mkdir ~/.vim/pack/vendor/Spacegray
+git clone git://github.com/ajh17/Spacegray.vim ~/.vim/pack/vendor/start/Spacegray
